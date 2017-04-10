9bc5fb3319cc45e3cb99791db9a2ccc2b81bf3f3
Ivan Seidel <ivanseidel@gmail.com>
2017-04-10 13:50:41 +0200

    Download CSV button

--- public/tournamenter-obr/scripts_rounds/app.js
+++ public/tournamenter-obr/scripts_rounds/app.js
@@ -163,6 +163,30 @@ var app = angular.module('app', [
     window.prompt('Tecle `ctrl + c` para copiar a tabela abaixo. Depois, cole em uma "MessageView" para mostrar as equipes os horários.', str)
   }
 
+  // Export CSV (Download)
+
+  $scope.downloadCSV = function (table, nivel, rodada) {
+    var csvFile = exportToCsv(table)
+    var filename = nivel + ': Rodada ' + rodada
+
+    var blob = new Blob([csvFile], { type: 'text/csv;charset=utf-8;' });
+    if (navigator.msSaveBlob) { // IE 10+
+        navigator.msSaveBlob(blob, filename);
+    } else {
+        var link = document.createElement("a");
+        if (link.download !== undefined) { // feature detection
+            // Browsers that support HTML5 download attribute
+            var url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", filename);
+            link.style.visibility = 'hidden';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    }
+  }
+
 
   // Parses a period in form of 'hh:mm-hh-mm'
   function parsePeriod (periodo) {
@@ -212,3 +236,29 @@ var app = angular.module('app', [
     return parts[0] * 60 + parts[1]
   }
 })
+
+function exportToCsv(rows) {
+    var processRow = function (row) {
+        var finalVal = '';
+        for (var j = 0; j < row.length; j++) {
+            var innerValue = row[j] === null ? '' : row[j].toString();
+            if (row[j] instanceof Date) {
+                innerValue = row[j].toLocaleString();
+            };
+            var result = innerValue.replace(/"/g, '""');
+            if (result.search(/("|,|\n)/g) >= 0)
+                result = '"' + result + '"';
+            if (j > 0)
+                finalVal += ',';
+            finalVal += result;
+        }
+        return finalVal + '\n';
+    };
+
+    var csvFile = '';
+    for (var i = 0; i < rows.length; i++) {
+        csvFile += processRow(rows[i]);
+    }
+
+    return csvFile
+}
