726c6bb2e50579a5c4cf0eb785b0b663e32f270b
Ivan Seidel <ivanseidel@gmail.com>
2017-04-16 10:47:53 -0300

    Add time progress bar

--- public/tournamenter-obr/scripts/scorers.js
+++ public/tournamenter-obr/scripts/scorers.js
@@ -51,6 +51,7 @@ var app = angular.module('app.scorers', [])
     view: 'views/rescue_scorer_2017_regional.html?r='+Math.random(),
     model: model,
     scorings: scorings,
+    totalTime: 60,
     score: function (model){
       var scored = {
         total: 0,
